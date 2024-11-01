5085b53c1b615ef2a32d840fba15cf3cb0426a0f
Alexandre GBAMELE <alexdelor2022@gmail.com>
2024-11-01 12:16:47 +0100

    first commit

---
+++ src/main/java/com/example/gestionEmployerBackend/GestionEmployerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionEmployerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmployerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEmployerBackendApplication.class, args);
+	}
+
+}
